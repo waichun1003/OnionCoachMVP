0ceec12db7122db1580ec942626c484133fb11d6
waichuncheng <waichun1003@gmail.com>
2025-04-25 18:02:52 +0800

    fix the prisma production configuration

--- onioncoachmvp/src/lib/prisma.ts
+++ onioncoachmvp/src/lib/prisma.ts
@@ -1,11 +1,16 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient }
+const prismaClientSingleton = () => {
+  return new PrismaClient().$extends(withAccelerate())
+}
 
-export const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({
-    log: ['query', 'error', 'warn'],
-  })
+declare global {
+  var prisma: undefined | ReturnType<typeof prismaClientSingleton>
+}
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+const prisma = globalThis.prisma ?? prismaClientSingleton()
+
+if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma
+
+export { prisma }
